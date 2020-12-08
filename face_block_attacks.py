54cff40b62e895e133ca99048dda13bc71399196
Darshan Thaker <dbthaker@gmail.com>
2020-12-07 19:50:51 -0800

    new criterion for eval + bug fixes

--- face_block_attacks.py
+++ face_block_attacks.py
@@ -5,14 +5,23 @@ import os
 import pickle
 import scipy.io
 import torch
+import seaborn as sns
 import torch.nn as nn
 import re
 import matplotlib.pyplot as plt
 
 from PIL import Image
 from cvxpy.atoms.norm import norm
+from sklearn.preprocessing import normalize
+from sklearn.neighbors import NearestNeighbors
 from neural_net import NN, CNN
 from pdb import set_trace
+
+SIGNAL = 'signal'
+ATTACK = 'attack'
+EPS = 10
+SIGNAL_BIDXS = list()
+ATTACK_BIDXS = list()
 
 class Trainer(object):
 
@@ -87,7 +96,6 @@ class Trainer(object):
 
     def compute_lp_dictionary(self, eps, lp, block=False):
         idx = list(range(self.N_train))
-        np.random.shuffle(idx)
         bsz = self.N_train
         dictionary = list()
         blocks = dict()
@@ -111,22 +119,32 @@ class Trainer(object):
                 print("Attack not supported!")
                 sys.exit(1)
             dictionary = bx_attack
+            for label in range(38):
+                blocks[label] = bx_attack[(by == label).nonzero().squeeze()]
             if block:
-                for label in range(38):
-                    blocks[label] = bx_attack[(by == label).nonzero().squeeze()]
                 return blocks
         return dictionary.T
 
-    def sample_attack(self, lp, ex_idx):
-        blocks = self.compute_lp_dictionary(eps=0.1, lp=lp, block=True)
+    # k-nearest neighbors
+    def sample_attack(self, lp, ex_idx, k):
+        blocks = self.compute_lp_dictionary(eps=EPS, lp=lp, block=True)
+
         D = [np.zeros(blocks[i].shape[0]) for i in range(38)]
         for i in range(10):
-            D[i][0] = 1
+            D[i][0] = 1./3.
+            D[i][1] = 1./3.
+            D[i][2] = 1./3.
         perturb = [np.dot(blocks[i].T, D[i]) for i in range(38)]
 
         bx = self.test_X[ex_idx, :, :]
         by = self.test_y[ex_idx]
         bx = bx.reshape(-1)
+
+        #neigh = NearestNeighbors(n_neighbors=k)
+        #neigh.fit(self.train_X.reshape((self.train_X.shape[0], -1)))
+        #dist, ind = neigh.kneighbors(bx.reshape((1, -1)))
+        #set_trace()
+
         perturbation = np.array(perturb[by])
         adv = bx + perturbation
         return adv
@@ -173,7 +191,7 @@ def parse_data(resize=True, normalize=False):
                 continue
             im_path = os.path.join(PATH, folder, im_name)
             if resize:
-                im = np.array(Image.open(im_path).resize((35, 40)))
+                im = np.array(Image.open(im_path).resize((35, 40), resample=Image.LANCZOS))
             else:
                 im = np.array(Image.open(im_path))
             if normalize:
@@ -193,7 +211,7 @@ def split_train_test(data):
             set_trace()
         indices = list(range(N))
         np.random.shuffle(indices)
-        m = int(0.7 * N)
+        m = 44 # ~70%
         train[pid] = data[pid][:m, :, :]
         test[pid] = data[pid][m:, :, :]
     return train, test
@@ -210,78 +228,118 @@ def to_mat():
     scipy.io.savemat('YaleBCrop.mat', {'I': data})
     set_trace()
 
-def lp_reconstruct(trainer, ex_idx, lp):
+def get_block(x, bidx, mode):
+    global SIGNAL_BIDXS, ATTACK_BIDXS
+    if mode == SIGNAL:
+        if len(x.shape) == 2:
+            return x[:, SIGNAL_BIDXS[bidx]:SIGNAL_BIDXS[bidx+1]]
+        else:
+            return x[SIGNAL_BIDXS[bidx]:SIGNAL_BIDXS[bidx+1]]
+    elif mode == ATTACK:
+        if len(x.shape) == 2:
+            return x[:, ATTACK_BIDXS[bidx]:ATTACK_BIDXS[bidx+1]]
+        else:
+            return x[ATTACK_BIDXS[bidx]:ATTACK_BIDXS[bidx+1]]
+
+def lp_reconstruct(trainer, ex_idx, lp, dict_in_obj=True, finegrained=False):
     print("-------------------{}-{}---------------------".format(ex_idx, lp))
-    l2_dict = trainer.compute_lp_dictionary(0.1, 2)
-    linf_dict = trainer.compute_lp_dictionary(0.1, np.infty)
-    #x_adv = trainer.sample_attack(np.infty)
-    x_adv = trainer.sample_attack(lp, ex_idx)
+    l2_dict = trainer.compute_lp_dictionary(EPS, 2)
+    linf_dict = trainer.compute_lp_dictionary(EPS, np.infty)
+    attack_dict = np.hstack([l2_dict, linf_dict])
+    attack_dict = normalize(attack_dict, axis=0)
+    x_adv = trainer.sample_attack(lp, ex_idx, k=5)
 
     data = parse_data()
     train, test = split_train_test(data)
+    global SIGNAL_BIDXS
+    SIGNAL_BIDXS = [0]
+    for i in range(len(train)):
+        SIGNAL_BIDXS.append(SIGNAL_BIDXS[-1] + train[i].shape[0])
     signal_dict = compute_dictionary(train)
+    signal_dict = normalize(signal_dict, axis=0)
 
-    concat_dict = np.hstack([signal_dict, l2_dict, linf_dict])
+    concat_dict = np.hstack([signal_dict, attack_dict])
 
     s_len, l2_len, linf_len = signal_dict.shape[1], l2_dict.shape[1], linf_dict.shape[1]
 
     attack_lens = [l2_len, linf_len]
+    # Attack comes after signal in concat_dict
+    global ATTACK_BIDXS
+    if finegrained:
+        ATTACK_BIDXS = [s_len + i for i in SIGNAL_BIDXS]
+        ATTACK_BIDXS += [s_len+l2_len + i for i in SIGNAL_BIDXS]
+    else: 
+        ATTACK_BIDXS = [s_len, s_len+l2_len, s_len+l2_len+linf_len]
 
     thres = 0.1
 
-    #c_l2 = cp.Variable(N)
     c = cp.Variable(s_len + sum(attack_lens))
-    #objective = cp.Minimize(norm(c_s, p=2) + cp.sum(norm(c_l2, p=2) + norm(c_linf, p=2)))
-    #objective = cp.Minimize(cp.sum(norm(c_l2, p=2) + norm(c_linf, p=2)))
 
     idx = 0
     objective = 0
     for pid in range(len(train)):
         l = train[pid].shape[0]
-        objective += norm(concat_dict[:, idx:idx+l]@c[idx:idx+l], p=2)
-        #objective += norm(c[idx:idx+l], p=2)
+        if dict_in_obj:
+            objective += norm(concat_dict[:, idx:idx+l]@c[idx:idx+l], p=2)
+        else:
+            objective += norm(c[idx:idx+l], p=2)
         idx += l
 
     for a in range(len(attack_lens)):
         a_len = attack_lens[a]
-        objective += norm(concat_dict[:, idx:idx+a_len]@c[idx:idx+a_len], p=2)
-        #objective += norm(c[idx:idx+a_len], p=2)
+        if dict_in_obj:
+            objective += norm(concat_dict[:, idx:idx+a_len]@c[idx:idx+a_len], p=2)
+        else:
+            objective += norm(c[idx:idx+a_len], p=2)
         idx += a_len
-    minimizer = cp.Minimize(objective + 0.1*norm(x_adv - concat_dict@c, p=2)) 
-    #constraints = [norm(x_adv - concat_dict@c, p=2) <= thres]
-    #constraints = [norm(x - signal_dict@c[:s_len], p=2) <= thres]
+    minimizer = cp.Minimize(objective)
+    constraints = [norm(x_adv - concat_dict@c, p=2) <= thres]
 
-    prob = cp.Problem(minimizer)
+    prob = cp.Problem(minimizer, constraints)
 
-    result = prob.solve(verbose=True, max_iters=10)
+    result = prob.solve(verbose=True, solver=cp.MOSEK)
 
     opt = np.array(c.value)
-    l2_coef = opt[s_len:s_len+l2_len]
-    linf_coef = opt[s_len+l2_len:s_len+l2_len+linf_len]
-    l2_coef_norm = np.linalg.norm(l2_dict@l2_coef, 2)
-    linf_coef_norm = np.linalg.norm(linf_dict@linf_coef, 2)
-    print("L2: {}. Linf: {}".format(l2_coef_norm, linf_coef_norm))
 
-    """
-    idx = 0
-    objective = 0
-    for pid in range(len(train)):
-        l = train[pid].shape[0]
-        objective += np.linalg.norm(concat_dict[:, idx:idx+l]@opt[idx:idx+l], 2)
-        idx += l
-    print("objective after signal: {}".format(objective))
-    for a in range(len(attack_lens)):
-        a_len = attack_lens[a]
-        objective += np.linalg.norm(concat_dict[:, idx:idx+a_len]@opt[idx:idx+a_len], 2)
-        idx += a_len
-        print("objective after attack: {}".format(objective))
-    print("Total: {}".format(objective + 0.1*np.linalg.norm(x_adv - concat_dict@opt, 2)))
-    """
+    eps = np.zeros((len(SIGNAL_BIDXS) - 1, len(ATTACK_BIDXS) - 1))
+    min_val = np.infty
+    argmin_val = None
+    for i in range(len(SIGNAL_BIDXS) - 1):
+        for j in range(len(ATTACK_BIDXS) - 1):
+            try:
+                assert s_len == l2_len and s_len == linf_len
+                attack_d = get_block(concat_dict, j, ATTACK)
+                attack_person_d = get_block(attack_d, i, SIGNAL)
+                attack_c = get_block(opt, j, ATTACK)
+                attack_person_c = get_block(attack_c, i, SIGNAL)
+                res = x_adv - \
+                      get_block(concat_dict, i, SIGNAL) @ get_block(opt, i, SIGNAL) - \
+                      attack_person_d @ attack_person_c 
+                      #get_block(concat_dict, j, ATTACK) @ get_block(opt, j, ATTACK)
+            except:
+                print("ERROR!")
+                set_trace()
+            eps[i][j] = np.linalg.norm(res, 2)
+            if eps[i][j] < min_val:
+                min_val = eps[i][j]
+                argmin_val = i, j
+    print(eps)
+    print("min_val at person {}, attack {}. value: {}".format(argmin_val[0], argmin_val[1], min_val))
+    c_a = opt[s_len:]
+    #plt.plot(c_a)
+    #plt.show()
 
-    if l2_coef_norm > linf_coef_norm:
-        print("It's an L2 attack!")
-    else:
-        print("It's an Linf attack!")
+    ax = sns.heatmap(eps, annot=False, fmt="0.0f",
+                xticklabels=range(1, eps.shape[1]+1), yticklabels=range(1, eps.shape[0]+1))
+    #plt.show()
+
+    perturb = 1./3 * linf_dict[:, 0] + 1./3 *linf_dict[:, 1] + 1./3*linf_dict[:, 2]
+    signal_res = x_adv - \
+        get_block(concat_dict, 0, SIGNAL) @ get_block(opt, 0, SIGNAL) - perturb
+    res = x_adv - \
+        get_block(concat_dict, 0, SIGNAL) @ get_block(opt, 0, SIGNAL) - \
+        get_block(concat_dict, 1, ATTACK) @ get_block(opt, 1, ATTACK)
+    set_trace()
 
 def reconstruct():
     # Adversarial Dictionary: D x N
@@ -289,8 +347,8 @@ def reconstruct():
     data = parse_data()
     train, test = split_train_test(data)
 
-    dog = np.array(Image.open('dog.png').resize((15, 15)).convert('L'))
-    mario = np.array(Image.open('mario.png').resize((15, 15)).convert('L'))
+    dog = np.array(Image.open('dog.png').resize((15, 15), resample=Image.LANCZOS).convert('L'))
+    mario = np.array(Image.open('mario.png').resize((15, 15), resample=Image.LANCZOS).convert('L'))
     x = test[0][0, :, :]
     x_adv = attack(x, dog, (0, 0)).reshape(-1)
 
@@ -299,6 +357,8 @@ def reconstruct():
 
     dog_dict = attack(np.zeros(x.shape), dog, (0, 0)).reshape((-1, 1))
     mario_dict = attack(np.zeros(x.shape), mario, (0, 0)).reshape((-1, 1))
+
+    adv_dict = np.hstack([dog_dict, mario_dict])
 
     # Signal Dictionary: D x n_atoms
     signal_dict = compute_dictionary(train)
@@ -344,10 +404,9 @@ def reconstruct():
 np.random.seed(0)
 trainer = Trainer()
 trainer.train(10, 1e-4) 
-print("P prime dict * c objective")
-lp_reconstruct(trainer, 0, 2)
-lp_reconstruct(trainer, 0, np.infty)
-lp_reconstruct(trainer, 10, 2)
-lp_reconstruct(trainer, 10, np.infty)
-lp_reconstruct(trainer, 20, 2)
-lp_reconstruct(trainer, 20, np.infty)
+#lp_reconstruct(trainer, 0, 2, dict_in_obj=False)
+lp_reconstruct(trainer, 0, np.infty, dict_in_obj=False)
+#lp_reconstruct(trainer, 10, 2)
+#lp_reconstruct(trainer, 10, np.infty)
+#lp_reconstruct(trainer, 20, 2)
+#lp_reconstruct(trainer, 20, np.infty)
