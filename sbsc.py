2d528e93b12be68a8d0c9d2a57959c8e07ab8039
Darshan Thaker <dbthaker@gmail.com>
2021-06-22 17:06:32 -0400

    checkpoint

--- sbsc.py
+++ sbsc.py
@@ -3,6 +3,7 @@ import cvxpy as cp
 import sys
 import os
 import pickle
+import time
 import scipy.io
 import torch
 import torchvision
@@ -21,6 +22,7 @@ from neural_net import NN, CNN
 from pdb import set_trace
 from scipy.linalg import orth
 from scipy.sparse.linalg import svds
+from sklearn.preprocessing import normalize
 from torchvision import transforms
 from torch.utils.data import DataLoader, Dataset
 from advertorch.attacks import SparseL1DescentAttack, L2PGDAttack, LinfPGDAttack
@@ -28,6 +30,7 @@ from baselines import evaluate_baseline
 from kymatio.torch import Scattering2D
 from trainer import Trainer
 from irls import BlockSparseIRLSSolver
+from active_set import BlockSparseActiveSetSolver
 
 def serialize_dictionaries(trainer, args):
     toolchain = args.toolchain
@@ -100,7 +103,7 @@ def baseline(trainer, test_adv, test_y):
             res.append(np.linalg.norm(x_adv - Ds[:, l*pid:l*(pid + 1)]@opt[l*pid:l*(pid + 1)]))
         pred.append(np.argmin(res))
     pred = np.array(pred)
-    acc = sum(pred == test_y)
+    acc = sum(pred == test_y) / test_y.shape[0] * 100.
     return acc
 
 def epoch_adversarial(loader, lr_schedule, model, epoch_i, attack, criterion = nn.CrossEntropyLoss(), 
@@ -139,14 +142,17 @@ def epoch_adversarial(loader, lr_schedule, model, epoch_i, attack, criterion = n
         
     return train_loss / train_n, train_acc / train_n, adv
 
-def sbsc(trainer, args, eps, test_lp):
+def sbsc(trainer, args, eps, test_lp, lp_variant, use_cnn_for_dict=False):
     toolchain = args.toolchain
     eps_map = utils.EPS[args.dataset]
     #eps = eps_map[args.test_lp]
 
     attack_dicts = list()
     for attack in toolchain:
-        attack_dicts.append(trainer.compute_lp_dictionary(eps_map[attack], attack))
+        if use_cnn_for_dict:
+            attack_dicts.append(trainer.compute_lp_dictionary(eps_map[attack], attack, net='cnn'))
+        else:
+            attack_dicts.append(trainer.compute_lp_dictionary(eps_map[attack], attack))
 
     np.random.seed(0)
     #test_idx = list(range(100))
@@ -160,7 +166,7 @@ def sbsc(trainer, args, eps, test_lp):
     sz = utils.SIZE_MAP[dst]
     num_attacks = len(toolchain)
     
-    test_adv = trainer.test_lp_attack(test_lp, test_x, test_y, eps, realizable=False)
+    test_adv = trainer.test_lp_attack(test_lp, test_x, test_y, eps, realizable=False, lp_variant=lp_variant)
 
     ####### MAINI CODE
     #trainer.load_model('Msd')
@@ -182,8 +188,8 @@ def sbsc(trainer, args, eps, test_lp):
     ########
     #bx = trainer.test_X[test_idx, :, :]
     #set_trace()
-    #acc = trainer.evaluate(given_examples=(test_adv, test_y))
-    #print("[L{}, eps={}] Adversarial accuracy: {}%".format(test_lp, eps, acc))
+    acc = trainer.evaluate(given_examples=(test_adv, test_y))
+    print("[L{}, variant={}, eps={}] Adversarial accuracy: {}%".format(test_lp, lp_variant, eps, acc))
 
     #models = ['L1', 'L2', 'Linf', 'Max', 'Avg', 'Msd', 'Vanilla']
     #for model in models:
@@ -199,63 +205,136 @@ def sbsc(trainer, args, eps, test_lp):
     class_preds = list()
     attack_preds = list()
     denoised = list()
-    num_examples = 5
+    mismatch = 0
+    num_examples = 10
     for t in range(num_examples):
         if t % 5 == 0:
             print(t)
-        raw_x = test_adv[t, :]
+        raw_x = test_x[t, :]
+        corrupted_x = test_adv[t, :]
+        # TMP
+        traw_x = torch.from_numpy(raw_x[None, :])
+        ty = torch.from_numpy(np.array(test_y[t:t+1]))
+        #x_Da = trainer._lp_attack(test_lp, eps, traw_x, ty, only_delta=True)
+        print("ty = {}, lp = {}".format(ty, test_lp))
+        #x_Da = x_Da.reshape(-1)
+        #raw_Da[:, 2000*toolchain.index(test_lp) + 200*ty+50] = x_Da
+        #Ds[:, 200*ty+50] = trainer.scattering(traw_x).cpu().detach().numpy().reshape(-1)
         if args.embedding == 'scattering':
             raw_x = torch.from_numpy(raw_x).unsqueeze(0)
+            cheat_x = torch.from_numpy(test_x[t, :]).unsqueeze(0)
+            tcorrupted_x = torch.from_numpy(corrupted_x).unsqueeze(0)
             if torch.cuda.is_available():
+                cheat_x = cheat_x.cuda()
                 raw_x = raw_x.cuda()
-            grad = torch.autograd.functional.jacobian(trainer.scattering.scattering, raw_x)
-            grad = grad.reshape(64*7*7, 28*28)
-            Da = grad.cpu() @ raw_Da
+            #grad = torch.autograd.functional.jacobian(trainer.scattering.scattering, raw_x)
+            #grad = grad.reshape(64*7*7, 28*28)
+            #cheat_grad = torch.autograd.functional.jacobian(trainer.scattering.scattering, cheat_x)
+            #cheat_grad = cheat_grad.reshape(64*7*7, 28*28)
+            #pickle.dump(cheat_grad, open('cheat_grad_{}.pkl'.format(t), 'wb'))
+            cheat_grad = pickle.load(open('cheat_grad_{}.pkl'.format(t), 'rb'))
+            # TODO: Use cheat_grad here and see if that approximation is to blame.
+            Da = cheat_grad.cpu() @ raw_Da
             Da = Da.numpy()
-            x = trainer.scattering(raw_x).cpu().detach().numpy().reshape(-1)
+            Ds = normalize(Ds, axis=0)
+            Da = normalize(Da, axis=0)
+            #pickle.dump(Da, open('Da_jacobian_ex1.pkl', 'wb'))
+            x = trainer.scattering(tcorrupted_x).cpu().detach().numpy().reshape(-1)
+            #scipy.io.savemat('mats/{}_ce/Da_jacobian_ex{}.mat'.format(dst, t), {'data': Da})
+            #scipy.io.savemat('mats/{}_ce/scattered_ex{}.mat'.format(dst, t), {'data': x})
         else:
             Da = raw_Da
-            x = raw_x.reshape(-1)
-        set_trace()
-        solver = BlockSparseIRLSSolver(Ds, Da, trainer.num_classes, num_attacks, sz, 
-                lambda1=args.lambda1, lambda2=args.lambda2, del_threshold=args.del_threshold)
-        cs_est, ca_est, Ds_est, Da_est, err_cs, ws, wa, active_classes = solver.solve(x, alg=args.regularizer)
+            Da = normalize(Da, axis=0)
+            x = corrupted_x.reshape(-1)
+            pickle.dump(Ds, open('Ds.pkl', 'wb'))
+            pickle.dump(Da, open('Da.pkl', 'wb'))
+            print("Serialized Ds, Da!")
+        if args.solver == 'irls':
+            solver = BlockSparseIRLSSolver(Ds, Da, trainer.num_classes, num_attacks, sz, 
+                    lambda1=args.lambda1, lambda2=args.lambda2, del_threshold=args.del_threshold)
+        else:
+            solver = BlockSparseActiveSetSolver(Ds, Da, trainer.num_classes, num_attacks, sz, 
+                    lambda1=args.lambda1, lambda2=args.lambda2)
+            active_classes = list(range(10))
+   
+        start = time.time()
+        if args.solver == 'irls':
+            cs_est, ca_est, Ds_est, Da_est, err_cs, ws, wa, active_classes = solver.solve(x, alg=args.regularizer)
+            print("Active classes: {}".format(active_classes))
+        elif args.solver == 'en_full':
+            cs_est, ca_est, Ds_est, Da_est = solver.solve(x, alg='en_full') 
+        elif args.solver == 'active':
+            cs_est, ca_est, class_idx, (tmp_class, attack_idx) = solver.solve(x, alg='Ds+Da')
+        elif args.solver == 'active_refined':
+            cs_est, ca_est, sig_active_set, att_active_set = solver.solve(x, alg='Ds+Da')
+            print("Sig active set: {}. Att active set: {}".format(sig_active_set, att_active_set))
+        end = time.time()
+        #print("Took {} seconds".format(end - start))
 
-        err_class = list()
-        for i in range(solver.sig_bi.num_blocks[0]):
-            Ds_blk = solver.sig_bi.get_block(Ds_est, i)
-            cs_blk = solver.sig_bi.get_block(cs_est, i)
-            err_class.append(np.linalg.norm(x - Ds_blk@cs_blk - Da_est@ca_est))
-        err_class = np.array(err_class)
-        i_star = np.argmin(err_class)
-        class_preds.append(active_classes[i_star])
+        if args.solver == 'active':
+            class_preds.append(class_idx)
+            attack_preds.append(attack_idx)
+        elif args.solver == 'active_refined':
+            sig_active_set = list(sig_active_set)
+            att_active_set = list(att_active_set)
+            err_class = list()
+            for i in sig_active_set:
+                Ds_blk = solver.sig_bi.get_block(Ds, i)
+                cs_blk = solver.sig_bi.get_block(cs_est, i)
+                # TODO: This shouldn't be full Da.
+                err_class.append(np.linalg.norm(x - Ds_blk@cs_blk - Da@ca_est))
+            err_class = np.array(err_class)
+            i_star = np.argmin(err_class)
+            class_preds.append(sig_active_set[i_star])
 
-        err_attack = list()
-        for j in range(num_attacks):
-            Da_blk = solver.hier_bi.get_block(Da_est, (i_star, j)) 
-            ca_blk = solver.hier_bi.get_block(ca_est, (i_star, j)) 
-            err_attack.append(np.linalg.norm(x - Ds_est@cs_est - Da_blk@ca_blk))
-        err_attack = np.array(err_attack)
-        j_star = np.argmin(err_attack)
-        attack_preds.append(j_star)
-        #Da_blk = solver.hier_bi.get_block(Da_est, (i_star, j_star)) 
-        #ca_blk = solver.hier_bi.get_block(ca_est, (i_star, j_star)) 
-        Ds_blk = solver.sig_bi.get_block(Ds_est, i_star)
-        cs_blk = solver.sig_bi.get_block(cs_est, i_star)
-        denoised.append(Ds_blk@cs_blk)
-        #denoised.append(x - Da_blk@ca_blk)
+            err_attack = list()
+            for j in att_active_set:
+                Da_blk = solver.hier_bi.get_block(Da, (i_star, j)) 
+                ca_blk = solver.hier_bi.get_block(ca_est, (i_star, j)) 
+                err_attack.append(np.linalg.norm(x - Ds@cs_est - Da_blk@ca_blk))
+            err_attack = np.array(err_attack)
+            j_star = np.argmin(err_attack)
+            attack_preds.append(att_active_set[j_star])
+            Ds_blk = solver.sig_bi.get_block(Ds, i_star)
+            cs_blk = solver.sig_bi.get_block(cs_est, i_star)
+            denoised.append(Ds_blk@cs_blk)
+        else:
+            err_class = list()
+            for i in range(solver.sig_bi.num_blocks[0]):
+                Ds_blk = solver.sig_bi.get_block(Ds_est, i)
+                cs_blk = solver.sig_bi.get_block(cs_est, i)
+                err_class.append(np.linalg.norm(x - Ds_blk@cs_blk - Da_est@ca_est))
+            err_class = np.array(err_class)
+            i_star = np.argmin(err_class)
+            class_preds.append(active_classes[i_star])
+
+            err_attack = list()
+            for j in range(num_attacks):
+                Da_blk = solver.hier_bi.get_block(Da_est, (i_star, j)) 
+                ca_blk = solver.hier_bi.get_block(ca_est, (i_star, j)) 
+                err_attack.append(np.linalg.norm(x - Ds_est@cs_est - Da_blk@ca_blk))
+            err_attack = np.array(err_attack)
+            j_star = np.argmin(err_attack)
+            attack_preds.append(j_star)
+            #Da_blk = solver.hier_bi.get_block(Da_est, (i_star, j_star)) 
+            #ca_blk = solver.hier_bi.get_block(ca_est, (i_star, j_star)) 
+            Ds_blk = solver.sig_bi.get_block(Ds_est, i_star)
+            cs_blk = solver.sig_bi.get_block(cs_est, i_star)
+            denoised.append(Ds_blk@cs_blk)
     class_preds = np.array(class_preds)
     attack_preds = np.array(attack_preds)
     print("Class preds: {}. Ground Truth: {}".format(class_preds, test_y[:num_examples]))
+    print("Attack preds: {}. Ground Truth: {}".format(attack_preds, toolchain.index(test_lp)))
     denoised = np.array(denoised)
-    signal_acc = np.sum(class_preds == test_y[:num_examples])
-    attack_acc = np.sum(attack_preds == toolchain.index(test_lp))
+    signal_acc = np.sum(class_preds == test_y[:num_examples]) / float(num_examples) * 100.
+    attack_acc = np.sum(attack_preds == toolchain.index(test_lp)) / float(num_examples) * 100.
     #if args.dataset == 'mnist':
     #    denoised = denoised.reshape((num_examples, 1, 28, 28))
     #denoised_acc = trainer.evaluate(given_examples=(denoised, test_y[:100]))
 
     print("Signal classification accuracy: {}%".format(signal_acc))
     print("Attack detection accuracy: {}%".format(attack_acc))
+    print("Mismatch: {}%".format(mismatch / float(num_examples) * 100.))
     #print("Denoised accuracy: {}%".format(denoised_acc))
 
 if __name__=='__main__':
--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 import math
 import argparse
 import os
+import random
 
 from PIL import Image
 from typing import Union, Tuple
@@ -25,7 +26,7 @@ EPS = {'mnist': {1: 10., \
         np.infty: 0.03}}
 STEP = {'mnist': {1: 0.8, \
         2: 0.1, \
-        np.infty: 0.01}, \
+        np.infty: 0.01},
         'cifar': {1: 1.0, \
         2: 0.02, \
         np.infty: 0.003}, \
@@ -38,7 +39,7 @@ SIZE_MAP = {'yale': 20, \
 
 def get_parser(parser):
     parser.add_argument('--dataset', default='mnist', type=str, help='Dataset to use for experiments')
-    parser.add_argument('--lr', default=0.01, type=float, help='Learning rate for training network')
+    parser.add_argument('--lr', default=0.05, type=float, help='Learning rate for training network')
     parser.add_argument('--num_epochs', default=5, type=int, help='Number of epochs to train network')
     parser.add_argument('--bsz', default=128, type=int, help='Batch size')
     parser.add_argument('--arch', default='carlini_cnn', type=str, help='Network architecture')
@@ -49,9 +50,11 @@ def get_parser(parser):
     parser.add_argument('--regularizer', default=4, type=int, help='Regularizer to use for IRLS')
     parser.add_argument('--toolchain', default=[1, 2, np.infty], nargs='+', type=float, help='Toolchain')
     parser.add_argument('--test_lp', default=2, type=float, help='Lp perturbation type to apply to test points')
+    parser.add_argument('--lp_variant', default=None, type=str, help='Lp perturbation type variant to apply to test points')
     parser.add_argument('--lambda1', default=5, type=float, help='Lambda1 for IRLS')
     parser.add_argument('--lambda2', default=15, type=float, help='Lambda2 for IRLS')
     parser.add_argument('--del_threshold', default=0.2, type=float, help='Del threshold for IRLS')
+    parser.add_argument('--solver', default='active', type=str, help='Solver to use')
     return parser 
 
 
@@ -100,7 +103,7 @@ def parse_yale_data(resize=True):
         test[pid] = data[pid][m:, :, :, :]
     return data, train, test
 
-def pgd_linf(model, X, y, epsilon=0.3, alpha=0.01, num_iter = 50, randomize = 0, restarts = 0, device = "cuda:1"):
+def pgd_linf(model, X, y, epsilon=0.3, alpha=0.01, num_iter = 50, randomize = 0, restarts = 0, device = "cuda:0"):
     """ Construct FGSM adversarial examples on the examples X"""
     # ipdb.set_trace()
    
@@ -142,6 +145,238 @@ def pgd_linf(model, X, y, epsilon=0.3, alpha=0.01, num_iter = 50, randomize = 0,
         #Edit Max Delta only for successful attacks        
         max_delta[incorrect] = delta.detach()[incorrect]
     return max_delta
+
+def l1_dir_topk(grad, delta, X, gap, k = 10) :
+    #Check which all directions can still be increased such that
+    #they haven't been clipped already and have scope of increasing
+    # ipdb.set_trace()
+    X_curr = X + delta
+    batch_size = X.shape[0]
+    channels = X.shape[1]
+    pix = X.shape[2]
+    # print (batch_size)
+    neg1 = (grad < 0)*(X_curr <= gap)
+#     neg1 = (grad < 0)*(X_curr == 0)
+    neg2 = (grad > 0)*(X_curr >= 1-gap)
+#     neg2 = (grad > 0)*(X_curr == 1)
+    neg3 = X_curr <= 0
+    neg4 = X_curr >= 1
+    neg = neg1 + neg2 + neg3 + neg4
+    u = neg.view(batch_size,1,-1)
+    grad_check = grad.view(batch_size,1,-1)
+    grad_check[u] = 0
+
+    kval = kthlargest(grad_check.abs().float(), k, dim = 2)[0].unsqueeze(1)
+    k_hot = (grad_check.abs() >= kval).float() * grad_check.sign()
+    return k_hot.view(batch_size, channels, pix, pix)
+
+
+def pgd_l1_topk(model, X,y, epsilon = 10, alpha = 0.8, num_iter = 50, k_map = 0, gap = 0.05, device = "cuda:0", restarts = 0, randomize = 0):
+    #Gap : Dont attack pixels closer than the gap value to 0 or 1
+    # ipdb.set_trace()
+    gap = gap
+    max_delta = torch.zeros_like(X)
+    if randomize:
+        delta = torch.from_numpy(np.random.laplace(size=X.shape)).float().to(device)
+        delta.data = delta.data*epsilon/norms_l1(delta.detach())
+        delta.requires_grad = True
+    else:
+        delta = torch.zeros_like(X, requires_grad = True)
+    alpha_l_1_default = alpha
+
+    for t in range (num_iter):
+        output = model(X+delta)
+        incorrect = output.max(1)[1] != y 
+        correct = (~incorrect).unsqueeze(1).unsqueeze(1).unsqueeze(1).float()
+        #Finding the correct examples so as to attack only them
+        loss = nn.CrossEntropyLoss()(model(X+delta), y)
+        loss.backward()
+        if k_map == 0:
+            k = random.randint(5,20)
+            alpha   = (alpha_l_1_default/k)
+        elif k_map == 1:
+            k = random.randint(10,40)
+            alpha   = (alpha_l_1_default/k)
+        else:
+            k = 10
+            alpha = alpha_l_1_default
+        delta.data += alpha*correct*l1_dir_topk(delta.grad.detach(), delta.data, X, gap,k)
+        if (norms_l1(delta) > epsilon).any():
+            delta.data = proj_l1ball(delta.data, epsilon, device)
+        delta.data = torch.min(torch.max(delta.detach(), -X), 1-X) # clip X+delta to [0,1] 
+        delta.grad.zero_() 
+
+    max_delta = delta.detach()
+
+    #Restarts    
+    for k in range(restarts):
+        delta = torch.rand_like(X,requires_grad = True)
+        delta.data = (2*delta.data - 1.0)
+        delta.data = delta.data*epsilon/norms_l1(delta.detach())
+        for t in range (num_iter):
+            output = model(X+delta)
+            incorrect = output.max(1)[1] != y 
+            correct = (~incorrect).unsqueeze(1).unsqueeze(1).unsqueeze(1).float()
+            #Finding the correct examples so as to attack only them
+            loss = nn.CrossEntropyLoss()(model(X+delta), y)
+            loss.backward()
+            if k_map == 0:
+                k = random.randint(5,20)
+                alpha   = (alpha_l_1_default/k)
+            elif k_map == 1:
+                k = random.randint(10,40)
+                alpha   = (alpha_l_1_default/k)
+            else:
+                k = 10
+                alpha = alpha_l_1_default
+            delta.data += alpha*correct*l1_dir_topk(delta.grad.detach(), delta.data, X, gap,k)
+            if (norms_l1(delta) > epsilon).any():
+                delta.data = proj_l1ball(delta.data, epsilon, device)
+            delta.data = torch.min(torch.max(delta.detach(), -X), 1-X) # clip X+delta to [0,1] 
+            delta.grad.zero_() 
+        output = model(X+delta)
+        incorrect = output.max(1)[1] != y
+        #Edit Max Delta only for successful attacks
+        max_delta[incorrect] = delta.detach()[incorrect]   
+
+    return max_delta
+
+def proj_l1ball(x, epsilon=10, device = "cuda:0"):
+#     print (epsilon)
+    # print (device)
+    assert epsilon > 0
+    # compute the vector of absolute values
+    u = x.abs()
+    if (u.sum(dim = (1,2,3)) <= epsilon).all():
+        # print (u.sum(dim = (1,2,3)))
+         # check if x is already a solution
+        return x
+
+    # v is not already a solution: optimum lies on the boundary (norm == s)
+    # project *u* on the simplex
+    y = proj_simplex(u, s=epsilon, device = device)
+    # compute the solution to the original problem on v
+    y *= x.sign()
+    y *= epsilon/norms_l1(y)
+    return y
+
+def proj_simplex(v, s=1, device = "cuda:0"):
+    assert s > 0, "Radius s must be strictly positive (%d <= 0)" % s
+    batch_size = v.shape[0]
+
+    # check if we are already on the simplex    
+    '''
+    #Not checking this as we are calling this from the previous function only
+    if v.sum(dim = (1,2,3)) == s and np.alltrue(v >= 0):
+        # best projection: itself!
+        return v
+    '''
+    # get the array of cumulative sums of a sorted (decreasing) copy of v
+    u = v.view(batch_size,1,-1)
+    n = u.shape[2]
+    u, indices = torch.sort(u, descending = True)
+    cssv = u.cumsum(dim = 2)
+    # get the number of > 0 components of the optimal solution
+    vec = u * torch.arange(1, n+1).float().to(device)
+    comp = (vec > (cssv - s)).float()
+
+    u = comp.cumsum(dim = 2)
+    w = (comp-1).cumsum(dim = 2)
+    u = u + w
+    rho = torch.argmax(u, dim = 2)
+    rho = rho.view(batch_size)
+    c = torch.FloatTensor([cssv[i,0,rho[i]] for i in range( cssv.shape[0]) ]).to(device)
+    c = c-s
+    # compute the Lagrange multiplier associated to the simplex constraint
+    theta = torch.div(c,(rho.float() + 1))
+    theta = theta.view(batch_size,1,1,1)
+    # compute the projection by thresholding v using theta
+    w = (v - theta).clamp(min=0)
+    return w
+
+def kthlargest(tensor, k, dim=-1):
+    val, idx = tensor.topk(k, dim = dim)
+    return val[:,:,-1], idx[:,:,-1]
+
+def norms(Z):
+    return Z.view(Z.shape[0], -1).norm(dim=1)[:,None,None,None]
+
+def norms_l2(Z):
+    return norms(Z)
+
+def norms_l2_squeezed(Z):
+    return norms(Z).squeeze(1).squeeze(1).squeeze(1)
+
+def norms_l1(Z):
+    return Z.view(Z.shape[0], -1).abs().sum(dim=1)[:,None,None,None]
+
+def norms_l1_squeezed(Z):
+    return Z.view(Z.shape[0], -1).abs().sum(dim=1)[:,None,None,None].squeeze(1).squeeze(1).squeeze(1)
+
+def norms_l0(Z):
+    return ((Z.view(Z.shape[0], -1)!=0).sum(dim=1)[:,None,None,None]).float()
+
+def norms_l0_squeezed(Z):
+    return ((Z.view(Z.shape[0], -1)!=0).sum(dim=1)[:,None,None,None]).float().squeeze(1).squeeze(1).squeeze(1)
+
+def norms_linf(Z):
+    return Z.view(Z.shape[0], -1).abs().max(dim=1)[0]
+
+def pgd_l2(model, X, y, epsilon=2.0, alpha=0.1, num_iter = 100, restarts = 0, device = "cuda:0", randomize = 0):
+    # ipdb.set_trace()
+    max_delta = torch.zeros_like(X)
+    if random:
+        delta = torch.rand_like(X, requires_grad=True) 
+        delta.data *= (2.0*delta.data - 1.0)
+        delta.data = delta.data*epsilon/norms_l2(delta.detach()) 
+    else:
+        delta = torch.zeros_like(X, requires_grad=True) 
+    for t in range(num_iter):
+        output = model(X+delta)
+        incorrect = output.max(1)[1] != y 
+        correct = (~incorrect).unsqueeze(1).unsqueeze(1).unsqueeze(1).float()
+        #Finding the correct examples so as to attack only them
+        loss = nn.CrossEntropyLoss()(model(X + delta), y)
+        loss.backward()
+        delta.data +=  correct*alpha*delta.grad.detach() / norms(delta.grad.detach())
+        delta.data *=  epsilon / norms(delta.detach()).clamp(min=epsilon)
+        delta.data =   torch.min(torch.max(delta.detach(), -X), 1-X) # clip X+delta to [0,1]     
+        delta.grad.zero_()  
+
+    max_delta = delta.detach()
+
+    #restarts
+
+    for k in range (restarts):
+        delta = torch.rand_like(X, requires_grad=True) 
+        delta.data *= (2.0*delta.data - 1.0)*epsilon 
+        delta.data /= norms(delta.detach()).clamp(min=epsilon)
+        for t in range(num_iter):
+            output = model(X+delta)
+            incorrect = output.max(1)[1] != y 
+            correct = (~incorrect).unsqueeze(1).unsqueeze(1).unsqueeze(1).float()
+            #Finding the correct examples so as to attack only them
+            loss = nn.CrossEntropyLoss()(model(X + delta), y)
+            loss.backward()
+            delta.data +=  correct*alpha*delta.grad.detach() / norms(delta.grad.detach())
+            delta.data *= epsilon / norms(delta.detach()).clamp(min=epsilon)
+            delta.data = torch.min(torch.max(delta.detach(), -X), 1-X) # clip X+delta to [0,1]
+            delta.grad.zero_()  
+
+        output = model(X+delta)
+        incorrect = output.max(1)[1] != y
+        #Edit Max Delta only for successful attacks
+        max_delta[incorrect] = delta.detach()[incorrect] 
+
+    return max_delta    
+
+def blocksoft_thres(vec, lam):
+    norm_vec = np.linalg.norm(vec)
+    if norm_vec <= lam:
+        return np.zeros(vec.shape)
+    else:
+        return (1 - lam/norm_vec)*vec
+
 
 class YaleDataset(Dataset):
 
